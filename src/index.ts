61019fb8bb5c851bb57fbd07badbf1b0f9b2edd3
Wayne Van Son <=>
2024-10-24 21:34:44 +1100

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Reveal from "reveal.js"
+import RevealHighlight from "reveal.js/plugin/highlight/highlight"
+import RevealMarkdown from "reveal.js/plugin/markdown/markdown"
+import "reveal.js/dist/reveal.css"
+import "reveal.js/dist/theme/night.css"
+import "reveal.js/plugin/highlight/monokai.css"
+
+Reveal.initialize({
+  plugins: [RevealHighlight, RevealMarkdown],
+})
